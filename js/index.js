c7d1fa464d08d74b81664c18b97031cd56f0af49
thavha <42216436+thavha@users.noreply.github.com>
2021-11-23 13:22:02 +0200

    trying to add grids

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 
 
-const displayTopBarMenu = () =>{
-    let topBarMenuId = document.getElementById("topBarMenu").style;
-    if(topBarMenuId.display === "none"){
-        document.getElementById("topBarMenu").style.display = "grid";
-    }else{
-        document.getElementById("topBarMenu").style.display = "none";
+//const displayTopBarMenu = () =>{
+// let topBarMenuId = document.getElementById("topBarMenu").style;
+//if(topBarMenuId.display === "none"){
+//    document.getElementById("topBarMenu").style.display = "grid";
+//}else{
+//     document.getElementById("topBarMenu").style.display = "none";
+// }
+//}
+function myFunction() {
+
+    var x = document.getElementById("myTopnav");
+
+    if (x.className === "header") {
+
+        x.className += " responsive";
+
+    } else {
+
+        x.className = "header";
+
     }
+
 }
